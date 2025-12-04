70b0c88a22a608da2930d6e97cc4da2aa497d41f
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2025-12-04 21:34:15 +0100

    Improve convolution

--- day-4/convolution.rkt
+++ day-4/convolution.rkt
@@ -6,23 +6,6 @@
 (define (array-h A) (vector-ref (array-shape A) 0))
 (define (array-w A) (vector-ref (array-shape A) 1))
 
-;; Safe access: returns 0 when outside bounds
-(define (A-ref-pad A i j)
-  (define H (array-h A))
-  (define W (array-w A))
-  (if (and (>= i 0) (< i H)
-           (>= j 0) (< j W))
-      (array-ref A (vector i j))
-      0))
-
-(define (idxs i j kh kw)
-    ;; kernel center
-    (define shape (vector kh kw))
-    (for*/array: #:shape shape
-                 ([ii (range i (+ i kw))]
-                  [jj (range j (+ j kh))])
-        (vector ii jj)))
-
 (define (convolve2d data kernel)
     (define H (array-h data))
     (define W (array-w data))
@@ -31,28 +14,34 @@
     (define KW (array-w kernel))
 
     ; kernel center
-    (define kcenter-i (quotient KH 2))
-    (define kcenter-j (quotient KW 2))
+    (define KH/2 (quotient KH 2))
+    (define KW/2 (quotient KW 2))
+
+    ;; Safe access: returns 0 when outside bounds
+    (define (ref-pad i j)
+        (if (and (>= i 0) (< i H)
+                 (>= j 0) (< j W))
+            (array-ref data (vector i j))
+            0))
 
     ; copy input matrix to a result matrix with padding
     (define padshape
-        (vector (+ H (* 2 kcenter-i)) (+ W (* 2 kcenter-j))))
+        (vector (+ H (* 2 KH/2)) (+ W (* 2 KW/2))))
     (define result (array->mutable-array (make-array padshape 0)))
-    ; (array-indexes-set! result (idxs 1 1 H W) data)
 
     (for* ([i (range 0 W)]
            [j (range 0 H)])
 
-        ; extract subarray from position i - kcenter-i, j - kcenter - j
-        (let* ([fromi (- i kcenter-i)]
-               [fromj (- j kcenter-j)]
-               [indexes (idxs i j KH KW)]
-               [value (A-ref-pad data i j)]
-               [buf (array-indexes-ref result indexes)])
-            (array-indexes-set! result indexes
+        ; extract subarray from position i - KH/2, j - kcenter - j
+        (let ([value (ref-pad  i j)]
+              [buf (array-slice-ref result (list (in-range i (+ i KH))
+                                                 (in-range j (+ j KW))))])
+            (array-slice-set! result
+                (list (in-range i (+ i KH))
+                      (in-range j (+ j KW)))
                 (array+ buf (array* kernel (array value))))))
-    
-    (array-indexes-ref result (idxs 1 1 H W))
+
+    (array-slice-ref result (list (in-range 1 (add1 H)) (in-range 1 (add1 W))))
 )
 
 (provide convolve2d)
