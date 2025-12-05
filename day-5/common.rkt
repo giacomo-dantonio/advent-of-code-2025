9509fd555d8590d724e591e2a9faa2ed87cf3c05
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2025-12-05 13:28:04 +0100

    solve day 5

---
+++ day-5/common.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(define (parse input)
+    (define (parse-ranges lines)
+        ; parse ranges to a list of pairs, sorted by their lowest extremes
+        (define (cmp lhs rhs) (< (car lhs) (car rhs)))
+        (define ranges (sort
+            (for/list ([line lines])
+                (match (string-split line "-")
+                    [(list from to)
+                        (list (string->number from) (string->number to))]))
+            cmp))
+
+        ; merge overlapping ranges
+        (reverse
+            (for/fold ([acc (take ranges 1)])
+                      ([range (cdr ranges)])
+                ; Check if overlap, if so merge the ranges
+                (let ([lfrom (caar acc)]
+                      [lto (cadar acc)]
+                      [rfrom (car range)]
+                      [rto (cadr range)])
+                    (if (<= rfrom lto)
+                        ; merge the ranges
+                        (cons (list (min lfrom rfrom) (max lto rto))
+                              (cdr acc))
+                        (cons range acc)
+                    )))))
+
+    (define parts (string-split input "\n\n"))
+
+    (define ranges (parse-ranges (string-split (car parts) "\n")))
+
+    (define ids
+        (sort 
+            (map string->number (string-split (cadr parts) "\n"))
+            <))
+    
+    (values ranges ids))
+
+(provide parse)
---
+++ day-5/part2.rkt
@@ -0,0 +1,13 @@
+#lang racket
+
+(require racket/file)
+(require "common.rkt")
+
+(define (solve input)
+    (define-values (ranges _) (parse input))
+    (for/fold ([partial 0])
+              ([r ranges])
+        (+ partial (- (cadr r) (car r)) 1)))
+
+(define input (string-trim (file->string "./day-5/input")))
+(printf "Solution: ~a\n" (solve input))
